e0065b310370818ef2d4fcb3fdc59df5c1212576
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-12-20 10:14:40 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refame::Application.config.secret_token = 'b01cdf8240a71ebc0b6b1a85f694e5670ef696befc59c079292efb3b7e85b503859b56ffe2ea9cf2ad709197e718b38959dcadeeccaca750a7cbb58dd9c7acbe'
