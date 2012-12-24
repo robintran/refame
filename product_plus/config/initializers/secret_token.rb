9e5e028128560355ccbdf45e3525051a6a900fb2
Prasad Madala <bhanuprasad143@gmail.com>
2012-12-24 12:59:51 +0700

    Add Product Home Page

---
+++ product_plus/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProductPlus::Application.config.secret_token = 'c241e976c15f3121c201de121208d0a413e5f950e453731c9d30c9e1d1f722685a846daf4c1b506d52338d41fd0eba49a1413d38f646a27cc55742da9119a9ed'
