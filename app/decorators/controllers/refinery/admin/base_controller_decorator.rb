435703933f19349595cd5ee80d422610cc748619
Prasad Madala <bhanuprasad143@gmail.com>
2012-12-25 13:53:20 +0700

    Add Home Page::Part

--- app/decorators/controllers/refinery/admin/base_controller_decorator.rb
+++ app/decorators/controllers/refinery/admin/base_controller_decorator.rb
@@ -2,5 +2,6 @@ Refinery::Admin::BaseController.class_eval do
   def require_refinery_users!
     false
   end
+
 end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Refame::Application.configure do
 
   # Raise exception on mass assignment protection for Active Record models
   config.active_record.mass_assignment_sanitizer = :strict
-
+  config.assets.paths << "#{Rails.root}/app/assets/videos"
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
