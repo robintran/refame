dd767f8875c4929dec074216ce939b87751dabe1
Prasad Madala <bhanuprasad143@gmail.com>
2012-12-28 16:19:54 +0700

    Change JS

--- app/assets/javascripts/components/popup.js
+++ app/assets/javascripts/components/popup.js
@@ -1,27 +1,26 @@
 $(document).ready(function () {
-  // $(".popups").click(function(){
-  //     $('#zoomContainer01').css({width:$(document).width(), height:$(document).height()}).show();
-  //     $(".lightBox").css("left",($(document).width()/2)-200);
-  //     $(".lightBox").fadeIn();
-  //   });
+  $(".popups").click(function(){
+      $('#zoomContainer01').css({width:$(document).width(), height:$(document).height()}).show();
+      $(".lightBox").css("left",($(document).width()/2)-200);
+      $(".lightBox").fadeIn();
+    });
 
   $('#zoomContainer01').click(function () {
-    // $(".lightBox").fadeOut();
-    // $('#zoomContainer01').hide();
-    $(".email-login-form").fadeIn();
     $('#zoomContainer01').show();
+    $(".email-login-form").fadeIn();
     return false;
   });
 
-  // $('.clrPopup').click(function () {
-  //   $(".lightBox").fadeOut();
-  //   $('#zoomContainer01').hide();
-  //   return false;
-  // });
+  $('.clrPopup').click(function () {
+    $(".lightBox").fadeOut();
+    $('#zoomContainer01').hide();
+    $(".email-login-form").fadeIn();
+    return false;
+  });
 
-  // $(window).resize(function() {
-  //    if (!$('#zoomContainer01').is(':hidden')){
-  //      $('#zoomContainer01').css({width:$(document).width(), height:$(document).height()}).show();
-  //    };
-  // });
+  $(window).resize(function() {
+     if (!$('#zoomContainer01').is(':hidden')){
+       $('#zoomContainer01').css({width:$(document).width(), height:$(document).height()}).show();
+     };
+  });
 });
